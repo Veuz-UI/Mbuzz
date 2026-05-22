00639b3e3718d179d6ba3287f0ea3fe39c9213ad
shereef <shareef@veuz.in>
2026-05-22 17:15:39 +0530

    style fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -448,7 +448,7 @@ $(document).ready(function () {
             600: { items: 1 },
             768: { items: 1.2 },
             1000: { items: 2 },
-            1400: { items: 2 }
+            1400: { items: 2.2 }
         }
     });
 
