a84c6529d7a25b1c88301e4e1ef360691a7c718c
shereef <shareef@veuz.in>
2026-06-18 19:59:03 +0530

    style fix 14

--- assets/js/script.js
+++ assets/js/script.js
@@ -527,7 +527,7 @@ $(document).ready(function () {
         margin: 15,
         nav: false,
         dots: false,
-        autoplay: false,
+        autoplay: true,
         smartSpeed: 700,
         responsive: {
             0: { items: 1 },
